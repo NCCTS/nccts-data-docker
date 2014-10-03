b9ae0eeb4a9049a7e27abccf6dab819012d54719
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-02 20:17:07 -0500

    split "clone site" logic into a separate script

---
+++ support/clone_site.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $HOME
+rm -rf site
+git clone https://github.com/NCCTS/nccts.org.git
+cd nccts.org
+latest_tag=$(git describe --tags 2>/dev/null)
+if [ -n "$latest_tag" ]; then
+    git checkout $latest_tag
+else
+    echo "no tags, sticking with HEAD on master..."
+fi
+cd ..
+mv nccts.org/site ./
+rm -rf nccts.org
